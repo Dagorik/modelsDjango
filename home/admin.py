613ae00eee32a63b8dd22f59b59122fa3a27bc81
Raul Amador M <dagorik@gmail.com>
2017-06-29 17:31:53 -0500

    choices

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Remainder)
-admin.site.register(Prioridades)
+admin.site.register(Prioridad)
 
--- home/models.py
+++ home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class Prioridades(models.Model):
+class Prioridad(models.Model):
     name = models.CharField(max_length=10, null=False)
     fecha_creacion = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return "El nombre es " + self.name
 
 class Remainder(models.Model):
 
@@ -16,11 +16,25 @@ class Remainder(models.Model):
         ("BJ", "Baja"),
     )
 
+    MEDIA_CHOICES = (
+    ('Audio', (
+            ('vinyl', 'Vinyl'),
+            ('cd', 'CD'),
+        )
+    ),
+    ('Video', (
+            ('vhs', 'VHS Tape'),
+            ('dvd', 'DVD'),
+        )
+    ),
+    ('unknown', 'Unknown'),
+)
+
     id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=60)
     descripcion = models.TextField()
-    prioridad = models.ForeignKey(Prioridades, related_name='remainder_prioridad')
-    prioridad2 = models.CharField(max_length=7, choices=PRIORIDADES)
+    prioridad = models.ForeignKey(Prioridad, related_name='remainder_prioridad')
+    prioridad2 = models.CharField(max_length=7, choices=MEDIA_CHOICES)
     fecha = models.DateTimeField()
 
     def __str__(self):
